182b2fdbe65acc71538346a31fbc4ea264abc6b3
KCantrell12 <kwcantrell12@gmail.com>
2016-09-08 11:25:28 -0700

    Walls and camera

--- Assets/scripts/PlayerController.cs
+++ Assets/scripts/PlayerController.cs
@@ -3,9 +3,22 @@ using System.Collections;
 
 public class PlayerController : MonoBehaviour {
 
-	void Update ()
+	public float speed;
+
+	private Rigidbody rb;
+
+	void Start ()
 	{
-		Input
+		rb = GetComponent<Rigidbody> ();
 	}
 
+	void FixedUpdate ()
+	{
+		float moveHorizontal = Input.GetAxis ("Horizontal");
+		float moveVertical = Input.GetAxis ("Vertical");
+
+		Vector3 movement = new Vector3 (moveHorizontal, 0.0f, moveVertical);
+
+		rb.AddForce (movement);
+	}
 }
